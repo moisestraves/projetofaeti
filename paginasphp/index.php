12268092e662195d9590569502515c8ae001d1fd
Moisés Traves <51205821+traves8@users.noreply.github.com>
2020-05-13 03:58:16 -0300

    Atualização de versão do Banner co
    Local de Vestibular

---
+++ paginasphp/index.php
@@ -0,0 +1,54 @@
+											
+						<!-- Banner Central do Site-->
+						<section>
+								<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+				  <div class="carousel-inner">
+
+				    <div class="carousel-item active">
+
+				      <img class="d-block w-100" src="img/vestibular2002.png" alt="Alunos ">
+				      
+				      <!-- Adicionando legenda no banner e Cor-->
+				      <div class="carousel-caption d-none d-md-block">
+				    <h2 href="#" class="p-1 mb-1 bg-dark  text-success"> FAÇA AGORA SEU VESTIBULAR ON-LINE SEM SAIR DE CASA </h2>
+				   
+				    <p><button type="button" formaction= "https://www.facebook.com/faculdademodulopaulista/"class="btn btn-primary">Vestibular Online</button></p>
+				  </div>
+
+				    </div>
+				    <!-- REVISAR A IMAGEM DESTE BANNER COM O PESSOAL DA CRIAÇÃO<div class="carousel-item">
+				      <img class="d-block w-100" src="img/pessoanova.jpg
+				      " alt="Segundo Slide">
+				    </div>-->
+				   
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="img/discente1triboalunos.png" alt="Terceiro Slide">
+				    </div>
+				  </div>
+				  <a class="carousel-control-prev" href="#" role="button" data-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Anterior</span>
+				  </a>
+				  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Próximo</span>
+				  </a>
+				</div>
+				 </section>
+						
+						<!-- Imagens do Meio do Site onde tem as informações dos cursos -->
+						<div class="conteiner">
+						
+						<div><img src="img\graduacao.png"> </div>
+						<div><p><img src="img\pos-graduacao.png"></p></div>
+						<div><img src="img\cursoead.png"> </div>
+						
+						</div>
+					
+				
+					</body>
+					<!-- CORRIGIR ESTA BARRA<article id="barra">
+					 <p><img src="img\camadabarrafinal.png" ></p>
+
+					 	
+					
---
+++ paginasphp/top.php
@@ -0,0 +1,46 @@
+	<!DOCTYPE html>
+						<html lang="pt-br">
+						<head>
+							<meta charset="utf-8">
+							<meta name="viewport" content="width=device-width, initial-scale=1.0">
+							<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+					       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+					       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+					       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+							<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"
+							<!-- Principal CSS do Bootstrap -->
+					        <link rel="stylesheet" type="text/css" href="css/style.css">
+						<title>FAETI - Faculdade de Educação e Tecnologia Iracema </title>
+						 </head>
+						
+						<body> 
+
+					 	<header>
+						
+						<div id="menutop">
+						
+						<ul> 
+							<a href="https://faeti.alunosnet.com"target="_blank"><li> Portal do Aluno |</li></a>
+							
+							<a href="https://faeti.alunosnet.com"target="_blank"><li> Portal do Professor |</li></a> 
+							<a href="https://ava.alunosnet.com/faeti"target="_blank"><li> AVA </li></a>
+						</ul>
+						
+						</div>
+
+						<!-- INICIO DO MENU SEGUNDARIO DO SITE-->
+						<nav id='meu-principal'>
+							
+							<ul>
+								<li><a href="index.html"><h1><img src="img\logotopo.png"></h1></a> </h1></li>
+								<a href="cursos-graduacao.html"><li>Bacharelado |</li></a>
+								<a href="cursos-tecnologia.html"><li>Tecnólogo |</li></a> 
+								<a href="cursos-posgraduacao.html"><li>Pós-Graduação |</li></a>
+									
+								<a href="https://ceqeduca.eadplataforma.com "target="_blank"><li>Extensão Univesitária | </li></a>
+
+								<a href="#"><li>Institucional</li></a>
+						</nav>
+						
+						</header>
+						<main>
